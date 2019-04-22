a4d0614d8771364f9ed1d2d96c2f9c11c6333f13
William Chan <wchan1992@gmail.com>
2019-04-21 19:55:39 -0700

    Implement using ES6 Class/Superclass methods

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor () {
+    super();
+    this.age = 10;
+    this.job = 'find pollen';
+    this.honeyPot = 0;
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
